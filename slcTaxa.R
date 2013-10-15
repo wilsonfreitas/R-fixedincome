f83954738fe050b809a222f216d1592f290c6473
wilsonfreitas <wilson.freitas@gmail.com>
2013-10-15 08:45:37 -0300

    Added script to capture IDs of BM&F curves

---
+++ slcTaxa.R
@@ -0,0 +1,6 @@
+
+library(XML)
+
+url <- 'http://www2.bmf.com.br/pages/portal/bmfbovespa/boletim1/TxRef1.asp'
+doc <- htmlTreeParse(url, useInternalNodes=TRUE)
+opt <- xpathSApply(doc, "//option/@value")
