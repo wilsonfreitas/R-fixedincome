e8c83d29cbd84ac5583c88a6d75c06ec8745344c
wilsonfreitas <wilson.freitas@gmail.com>
2013-11-04 07:38:59 -0200

    Organizing docstrings

--- R/dib.R
+++ R/dib.R
@@ -1,8 +1,7 @@
 
-#' @title dib
-#' 
-#' @description
 #' Days in base
+#' 
+#' Annual rates must have an amount of days to compound
 #' 
 #' @export dib
 dib <- function(object, ...) UseMethod('dib', object)
