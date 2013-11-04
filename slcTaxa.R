25a7c2da59dc8ee5855cecf8eebe855201bb98b1
wilsonfreitas <wilson.freitas@gmail.com>
2013-11-04 07:41:14 -0200

    Display result

--- slcTaxa.R
+++ slcTaxa.R
@@ -4,3 +4,4 @@ library(XML)
 url <- 'http://www2.bmf.com.br/pages/portal/bmfbovespa/boletim1/TxRef1.asp'
 doc <- htmlTreeParse(url, useInternalNodes=TRUE)
 opt <- xpathSApply(doc, "//option/@value")
+opt
