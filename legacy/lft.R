eeda884318aca0f4bd663995a6e3dfeb4966244e
wilsonfreitas <wilson.freitas@gmail.com>
2014-07-09 05:04:09 -0300

    Moved files to legacy

--- legacy/lft.R
+++ legacy/lft.R
@@ -7,19 +7,14 @@ library(Quandl)
 library(fixedincome)
 library(bizdays)
 
-options(digits=10)
-
-truncate <- function(x, n=0) {
-  k <- 10^n
-  trunc(x*k)/k
-}
+truncate <- function(x, n=0) trunc(x*10^n)/(10^n)
 
 refdate <- as.Date('2014-03-21')
 cal <- Calendar(holidays=holidaysANBIMA, name='ANBIMA', weekdays=c('saturday', 'sunday'))
 dc <- as.daycount('business/252')
 disc <- as.compounding('discrete')
-selic <- Quandl('BCB/1178', 'raw', '2000-07-01', as.character(refdate), sort='asc')
 
+selic <- Quandl('BCB/1178', 'raw', '2000-07-01', as.character(refdate), sort='asc')
 selic <- within(selic, {
 	Date <- as.Date(Date)
 	Rate <- as.spotrate(Value/100, disc, dc, cal)
@@ -32,26 +27,21 @@ selic <- within(selic, {
 head(selic)
 tail(selic)
 
-# 6.170,327629 VNA 2014-06-17
-
 # Pricing LFT Instruments
-
 tit_pub <- read.table(format(refdate, 'ms%y%m%d.txt'), skip=2, sep='@', header=TRUE)
 lft_quot <- subset(tit_pub, Titulo == 'LFT',
 	select=c('Data.Referencia', 'Data.Vencimento', 'Tx..Indicativas', 'PU'))
 names(lft_quot) <- c('RefDate', 'Maturity', 'Spread', 'SpotPrice')
 
+VNA <- with(selic, VNA[Date == refdate])
 lft_quot <- within(lft_quot, {
 	Spread <- as.numeric(sub(',', '.', Spread))
 	Spread <- as.spotrate(Spread/100, disc, dc, cal)
 	SpotPrice <- as.numeric(sub(',', '.', SpotPrice))
 	RefDate <- as.Date(as.character(RefDate), format='%Y%m%d')
 	Maturity <- as.Date(as.character(Maturity), format='%Y%m%d')
-	# MaturityAdj <- adjust.next(Maturity, cal)
-	Quotation <- truncate(100/compound(Spread, from=RefDate, to=adjust.next(Maturity, cal)), 4)
-	VNA <- with(selic, VNA[Date == refdate])
+	Quotation <- truncate(100*discount(Spread, from=RefDate, to=adjust.next(Maturity, cal)), 4)
 	TheoPrice <- truncate(VNA*Quotation/100, 6)
-	rm(VNA)
 })
 
 lft_quot
