f217dd388e28469ad2c3ee66f7d9bedb487a98d8
wilsonfreitas <wilson.freitas@gmail.com>
2022-02-18 09:25:50 -0300

    Split the constructor into two different methods for numeric and SpotRate objects

--- R/spotratecurve-class.R
+++ R/spotratecurve-class.R
@@ -10,41 +10,57 @@ setClass(
     len_check <- length(object@terms) == length(object@.Data)
     unique_check <- length(object@terms) == length(unique(object@terms))
     terms_positivity_check <- all(object@terms > 0)
-    len_check & unique_check & terms_positivity_check
+    terms_na_check <- ! any(is.na(object@terms))
+    len_check & unique_check & terms_positivity_check & terms_na_check
   },
   contains = "SpotRate"
 )
 
 #' @export
-spotratecurve <- function(.value, .terms, .compounding, .daycount,
-                          .calendar = "actual", .refdate = Sys.Date(),
-                          .copyfrom = NULL) {
-  if (length(.value) != length(.terms))
-    stop("length(.value) must match length(.terms)")
-  .underlying <- 
-    if (is(.value, "SpotRate")) {
-      spotrate(.value = .value@.Data,
-               .compounding = .value@compounding,
-               .daycount = .value@daycount,
-               .calendar = .value@calendar,
-               .copyfrom = .copyfrom)
-    } else {
-      spotrate(.value = .value,
-               .compounding = .compounding,
-               .daycount = .daycount,
-               .calendar = .calendar,
-               .copyfrom = .copyfrom)
-    }
+spotratecurve <- function(x, terms, ..., refdate = Sys.Date()) {
+  UseMethod("spotratecurve")
+}
+
+#' @export
+spotratecurve.numeric <- function(x, terms, .compounding, .daycount,
+                                  .calendar = "actual", refdate = Sys.Date(),
+                                  .copyfrom = NULL) {
+  .underlying <- spotrate(.value = x,
+                          .compounding = .compounding,
+                          .daycount = .daycount,
+                          .calendar = .calendar,
+                          .copyfrom = .copyfrom)
+  
   .Object <- new("SpotRateCurve",
-                 .Data = .underlying,
+                 .Data = .underlying@.Data,
                  compounding = .underlying@compounding,
                  daycount = .underlying@daycount,
                  calendar = .underlying@calendar,
-                 terms = .terms,
-                 refdate = .refdate)
+                 terms = terms,
+                 refdate = refdate)
   
-  ix <- order(.terms)
-  .Object@terms <- .terms[ix]
+  ix <- order(terms)
+  .Object@terms <- term(terms[ix], "days")
+  .Object@.Data <- .Object@.Data[ix]
+  validObject(.Object)
+  .Object
+}
+
+#' @export
+spotratecurve.SpotRate <- function(x, terms,
+                                   refdate = Sys.Date(),
+                                  .copyfrom = NULL) {
+  
+  .Object <- new("SpotRateCurve",
+                 .Data = x@.Data,
+                 compounding = x@compounding,
+                 daycount = x@daycount,
+                 calendar = x@calendar,
+                 terms = terms,
+                 refdate = refdate)
+  
+  ix <- order(terms)
+  .Object@terms <- term(terms[ix], "days")
   .Object@.Data <- .Object@.Data[ix]
   validObject(.Object)
   .Object
@@ -64,8 +80,39 @@ setMethod(
 #' @export
 setMethod(
   "[",
+  signature(x = "SpotRateCurve", i = "logical", j="missing"),
+  function(x, i, j, ..., drop = TRUE) {
+    spotratecurve(x@.Data[i], x@terms[i], x@compounding, x@daycount, x@calendar)
+  }
+)
+
+#' @export
+setMethod(
+  "[",
   signature(x = "SpotRateCurve", i = "missing", j = "missing"),
   function(x, i, j, ..., drop = TRUE) {
+    spotratecurve(x@.Data, x@terms, x@compounding, x@daycount, x@calendar)
+  }
+)
+
+#' @export
+setReplaceMethod(
+  "[",
+  signature(x="SpotRateCurve", i="logical", j="missing", value="numeric"),
+  function(x, i, j, ..., value) {
+    if (any(i > length(x@.Data)) || any(i < 1))
+      stop("Index out of limits")
+    x@.Data[i] <- value
+    x
+  }
+)
+
+#' @export
+setReplaceMethod(
+  "[",
+  signature(x="SpotRateCurve", i="numeric", j="missing", value="SpotRate"),
+  function(x, i, j, ..., value) {
+    x[i] <- value@.Data
     x
   }
 )
@@ -83,27 +130,6 @@ setMethod(
       ix <- i
     }
     spotratecurve(x@.Data[mx], ix, x@compounding, x@daycount, x@calendar)
-  }
-)
-
-#' @export
-setMethod(
-  "[",
-  signature(x = "SpotRateCurve", i = "logical", j="missing"),
-  function(x, i, j, ..., drop = TRUE) {
-    spotratecurve(x@.Data[i], x@terms[i], x@compounding, x@daycount, x@calendar)
-  }
-)
-
-#' @export
-setReplaceMethod(
-  "[",
-  signature(x="SpotRateCurve", i="numeric", j="missing", value="numeric"),
-  function(x, i, j, ..., value) {
-    if (any(i > length(x@.Data)) || any(i < 1))
-      stop("Index out of limits")
-    x@.Data[i] <- value
-    x
   }
 )
 
@@ -133,6 +159,8 @@ setReplaceMethod(
   "[[",
   signature(x="SpotRateCurve", i="numeric", j="missing", value="SpotRate"),
   function(x, i, j, ..., value) {
+    warn_if_spotrate_slots_differ(x, value,
+                                  "Given SpotRate objects have different slots")
     contained_from <- i %in% x@terms
     contained_to <- x@terms %in% i
     if (any(contained_from)) {
@@ -151,11 +179,38 @@ setReplaceMethod(
 
 #' @export
 setReplaceMethod(
-  "[",
-  signature(x="SpotRateCurve", i="numeric", j="missing", value="SpotRate"),
+  "[[",
+  signature(x="SpotRateCurve", i="missing", j="missing", value="SpotRateCurve"),
   function(x, i, j, ..., value) {
-    x[i] <- value@.Data
+    warn_if_spotrate_slots_differ(x, value,
+                                  "Given SpotRate objects have different slots")
+    contained_from <- value@terms %in% x@terms
+    contained_to <- x@terms %in% value@terms
+    if (any(contained_from)) {
+      x@.Data[contained_to] <- value@.Data[contained_from]
+      x@terms[contained_to] <- value@terms[contained_from]
+    }
+    if (any(! contained_from)) {
+      value_ <- c(x@.Data, value@.Data)
+      terms_ <- c(x@terms, value@terms)
+      ix <- order(terms_)
+      x@.Data <- value_[ix]
+      x@terms <- terms_[ix]
+    }
     x
+  }
+)
+
+#' @export
+setMethod(
+  "as.data.frame",
+  signature(x = "SpotRateCurve"),
+  function(x, row.names = NULL, optional = FALSE, ...) {
+    spotrate_ <- spotrate(.value = x@.Data,
+                          .compounding = x@compounding,
+                          .daycount = x@daycount,
+                          .calendar = x@calendar)
+    data.frame(terms = x@terms, rates = spotrate_)
   }
 )
 
