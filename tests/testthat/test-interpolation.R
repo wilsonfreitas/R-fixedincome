71d4bed360df0ee8fd109d9e0b535cde0fbbec6b
wilsonfreitas <wilson.freitas@gmail.com>
2022-03-02 06:35:45 -0300

    Tests updated

--- tests/testthat/test-interpolation.R
+++ tests/testthat/test-interpolation.R
@@ -3,7 +3,7 @@ terms <- c(1, 11, 26, 27, 28)
 rates <- c(0.0719, 0.056, 0.0674, 0.0687, 0.07)
 
 test_that("it should create an interpolation class", {
-  interp <- interpolation("flatforward")
+  interp <- interp_flatforward()
   expect_s4_class(interp, "Interpolation")
   expect_equal(as.character(interp), "flatforward")
   expect_equal(interp@.Data, "flatforward")
@@ -11,7 +11,7 @@ test_that("it should create an interpolation class", {
 })
 
 test_that("it should prepare an interpolation with a curve", {
-  interp <- interpolation("linear")
+  interp <- interp_linear()
   curve <- spotratecurve(rates, terms, "simple", "actual/365", "actual")
   prep_interp <- prepare_interpolation(interp, curve)
   expect_s4_class(prep_interp, "Interpolation")
