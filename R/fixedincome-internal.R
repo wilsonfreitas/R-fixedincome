2fad0fb7b588c657cab7c380a9f59cfb555c34cf
wilsonfreitas <wilson.freitas@gmail.com>
2022-03-07 17:55:21 -0300

    Code formatting

--- R/fixedincome-internal.R
+++ R/fixedincome-internal.R
@@ -1,14 +1,14 @@
 
 #' Fixed income models, calculations and data structures
-#' 
+#'
 #' The \code{fixedincome} package brings a set of funtions that helps with the
 #' mathematics of interest rates and fixed income.
-#' It handles the interest rates and term structures of interest rates as objects and 
-#' provides many methods to tackle specific issues
-#' like computing discount factors and forward rates, interpolate term structures,
-#' fit curve models and so much more.
-#' This package also supports methods and models commonly used by practitioners to 
-#' do fixed income calculations.
+#' It handles the interest rates and term structures of interest rates as
+#' objects and provides many methods to tackle specific issues
+#' like computing discount factors and forward rates, interpolate term
+#' structures, fit curve models and so much more.
+#' This package also supports methods and models commonly used by practitioners
+#' to do fixed income calculations.
 #'
 #' @name fixedincome-package
 #' @aliases fixedincome
@@ -23,10 +23,10 @@ NULL
 
 
 #' @title Datasets
-#' 
+#'
 #' @description
 #' Interest rate datasets
-#' 
+#'
 #' @docType data
 #' @keywords datasets
 #' @name datasets
@@ -34,8 +34,7 @@ NULL
 
 #' @details
 #' \code{ZeroCurveBRL} Brazil's zero curve
-#' 
+#'
 #' @rdname datasets
 #' @name ZeroCurveBRL
 NULL
-
