23e8b0e8c682c543cbc814bacfe749d91670f8a2
wilsonfreitas <wilson.freitas@gmail.com>
2022-03-08 14:22:42 -0300

    Implementing good practices
    Changed library call with requireNamespace and
    message to packageStartupMessage.
    
    The points raised Notes on CRAN check.

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,10 @@
 
 .onAttach <- function(libname, pkgname) {
-  suppressMessages(suppressWarnings(library(bizdays)))
-
-  # check if bizdays has actual calendar
-  if (!"actual" %in% names(calendars())) {
-    bizdays::create.calendar("actual")
-    message("Created \"actual\" calendar")
+  if (requireNamespace("bizdays", quietly = TRUE)) {
+    # check if bizdays has actual calendar
+    if (!"actual" %in% names(calendars())) {
+      bizdays::create.calendar("actual")
+      packageStartupMessage("Created \"actual\" calendar")
+    }
   }
 }
