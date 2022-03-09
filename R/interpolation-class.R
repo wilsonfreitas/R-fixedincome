b1de846efcbd4baf9938099fc4e5c7a58a01a837
wilsonfreitas <wilson.freitas@gmail.com>
2022-03-09 17:26:24 -0300

    Documented Interpolation class and constructors

--- R/interpolation-class.R
+++ R/interpolation-class.R
@@ -1,7 +1,19 @@
 #' Interpolation classes
 #'
-#' Classes that implement interpolation methods to be used with SpotRateCurve
+#' Classes that implement interpolation methods to be used with `SpotRateCurve`
 #' objects.
+#'
+#' \itemize{
+#'   \item FlatForward
+#'   \item Linear
+#'   \item LogLinear
+#'   \item NaturalSpline
+#'   \item HermiteSpline
+#'   \item MonotoneSpline
+#'   \item NelsonSiegel
+#'   \item NelsonSiegelSvensson
+#' }
+#'
 #' Every class that implement a interpolation method inherits the
 #' \code{Interpolation} class.
 #'
@@ -80,6 +92,17 @@ setClass(
   contains = "Interpolation"
 )
 
+#' Interpolates a `SpotRateCurve`
+#'
+#' This method is internally used by the interpolation engine.
+#' The interpolation function is created by calling `prepare_interpolation`
+#' method.
+#' This method creates the interpolation funcion with the current state of the
+#' curve object and sets this function to the `func` slot.
+#'
+#' @keywords internal
+#' @aliases interpolate,Interpolation,numeric-method
+#'
 #' @export
 setGeneric(
   "interpolate",
@@ -88,7 +111,6 @@ setGeneric(
   }
 )
 
-#' @export
 setMethod(
   "interpolate",
   signature(object = "Interpolation", x = "numeric"),
@@ -107,24 +129,78 @@ setMethod(
   }
 )
 
+#' Create Interpolation objects
+#'
+#' Functions to create intepolation objects.
+#'
+#' @param beta1 a single numeric
+#' @param beta2 a single numeric
+#' @param beta3 a single numeric
+#' @param beta4 a single numeric
+#' @param lambda1 a single numeric
+#' @param lambda2 a single numeric
+#'
+#' @details
+#' `interp_flatforward` creates a `FlatForward` interpolation object.
+#'
+#' `interp_linear` creates a `Linear` interpolation object.
+#'
+#' `interp_loglinear` creates a `LogLinear` interpolation object.
+#'
+#' `interp_naturalspline` creates a `NaturalSpline` interpolation object.
+#'
+#' `interp_hermitespline` creates a `HermiteSpline` interpolation object.
+#'
+#' `interp_monotonespline` creates a `MonotoneSpline` interpolation object.
+#'
+#' `interp_nelsonsiegel` creates a `NelsonSiegel` interpolation object.
+#' The arguments `beta1`, `beta2`, `beta3`, `lambda1` are the paremeters of
+#' the Nelson-Siegel model for term structure.
+#'
+#' `interp_nelsonsiegelsvensson` creates a `NelsonSiegelSvensson`
+#' interpolation object.
+#' The arguments `beta1`, `beta2`, `beta3`, `beta4`, `lambda1`, `lambda2` are
+#' the paremeters of Svensson's extension to Nelson-Siegel the model for
+#' term structure.
+#'
+#' @name interpolation-constructor
+#'
+#' @examples
+#' terms <- c(1, 11, 26, 27, 28)
+#' rates <- c(0.0719, 0.056, 0.0674, 0.0687, 0.07)
+#'
+#' curve <- spotratecurve(rates, terms, "discrete", "actual/365", "actual")
+#'
+#' interpolation(curve) <- interp_flatforward()
+#'
+#' curve[[1:10]]
+NULL
+
+#' @rdname interpolation-constructor
 #' @export
 interp_flatforward <- function() new("FlatForward", "flatforward")
 
+#' @rdname interpolation-constructor
 #' @export
 interp_linear <- function() new("Linear", "linear")
 
+#' @rdname interpolation-constructor
 #' @export
 interp_loglinear <- function() new("LogLinear", "loglinear")
 
+#' @rdname interpolation-constructor
 #' @export
 interp_naturalspline <- function() new("NaturalSpline", "naturalspline")
 
+#' @rdname interpolation-constructor
 #' @export
 interp_hermitespline <- function() new("HermiteSpline", "hermitespline")
 
+#' @rdname interpolation-constructor
 #' @export
 interp_monotonespline <- function() new("MonotoneSpline", "monotonespline")
 
+#' @rdname interpolation-constructor
 #' @export
 interp_nelsonsiegel <- function(beta1, beta2, beta3, lambda1) {
   new("NelsonSiegel", "nelsonsiegel",
@@ -135,6 +211,7 @@ interp_nelsonsiegel <- function(beta1, beta2, beta3, lambda1) {
   )
 }
 
+#' @rdname interpolation-constructor
 #' @export
 interp_nelsonsiegelsvensson <- function(beta1, beta2, beta3, beta4,
                                         lambda1, lambda2) {
