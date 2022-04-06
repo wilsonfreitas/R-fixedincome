a6964ac058c4c03b900741a700c7f18529dcd6b7
wilsonfreitas <wilson.freitas@gmail.com>
2022-04-06 15:37:38 -0300

    Testing COPOMScenarios
    The class is done and works as desired.

--- examples/ffcopom_scenarios.R
+++ examples/ffcopom_scenarios.R
@@ -2,58 +2,61 @@
 source("examples/utils-functions.R")
 
 setClass(
-    "COPOMScenarios",
-    slots = c(
-        copom_dates = "ANY",
-        copom_moves = "numeric"
-    ),
-    contains = "Interpolation"
+  "COPOMScenarios",
+  slots = c(
+    copom_dates = "ANY",
+    copom_moves = "numeric"
+  ),
+  contains = "Interpolation"
 )
 
 interp_copomscenarios <- function(copom_dates, copom_moves) {
-    new("COPOMScenarios", "copomscenarios",
-        copom_dates = copom_dates,
-        copom_moves = copom_moves
-    )
+  obj <- new("COPOMScenarios", "copomscenarios",
+    copom_dates = copom_dates,
+    copom_moves = copom_moves
+  )
+  obj@propagate <- FALSE
+  obj
 }
 
 setMethod(
-    "prepare_interpolation",
-    signature(object = "COPOMScenarios", x = "SpotRateCurve"),
-    function(object, x, ...) {
-        x@interpolation <- NULL
+  "prepare_interpolation",
+  signature(object = "COPOMScenarios", x = "SpotRateCurve"),
+  function(object, x, ...) {
+    x@interpolation <- NULL
 
-        t1 <- term(bizdays(x@refdate, object@copom_dates, x@calendar), "days")
-        t1 <- c(t1, x@terms[x@terms > max(t1)][1])
-        t <- t1 - shift(t1, fill = 0)
+    t1 <- term(bizdays(x@refdate, object@copom_dates, x@calendar), "days")
+    t1 <- c(t1, x@terms[x@terms > max(t1)][1])
+    t <- t1 - shift(t1, fill = 0)
 
-        acc_moves <- c(0, cumsum(object@copom_moves))
-        comp <- cumprod(compound(as.spotrate(x[1]) + acc_moves, t))
+    acc_moves <- c(0, cumsum(object@copom_moves))
+    comp <- cumprod(compound(as.spotrate(x[1]) + acc_moves, t))
 
-        ix <- x@terms > max(t1)
-        terms <- c(term(1, "days"), t1, x@terms[ix])
-        prices <- c(compound(x[1]), comp, compound(x[ix]))
+    ix <- x@terms > max(t1)
+    terms <- c(term(1, "days"), t1, x@terms[ix])
+    prices <- c(compound(x[1]), comp, compound(x[ix]))
 
-        interp_coords <- xy.coords(terms, log(prices))
-        interp_fun <- approxfun(interp_coords, method = "linear")
-        dc <- x@daycount
-        comp <- x@compounding
-        object@func <- function(term) {
-            log_price <- interp_fun(term)
-            price <- exp(log_price)
-            rates(comp, toyears(dc, term, "days"), price)
-        }
-        object
+    interp_coords <- xy.coords(terms, log(prices))
+    interp_fun <- approxfun(interp_coords, method = "linear")
+    dc <- x@daycount
+    comp <- x@compounding
+    object@func <- function(term) {
+      log_price <- interp_fun(term)
+      price <- exp(log_price)
+      rates(comp, toyears(dc, term, "days"), price)
     }
+    object
+  }
 )
 
 
-di1 <- get_curve_from_web("2022-04-01")
+di1 <- get_curve_from_web("2022-04-04") |> fixedincome::first("2 years")
 cd <- copom_dates[copom_dates > di1@refdate][1:6]
-cm <- c(100, 100, 50, 50, 0, 0) / 1e4
+cm <- c(100, 25, 0, 0, 0, 0) / 1e4
 interpolation(di1) <- interp_copomscenarios(cd, cm)
-
 di1[[1:100]]
+plot(di1, use_interpolation = TRUE)
+
 
 # fixedincome::first(di1, "2 years")
 
